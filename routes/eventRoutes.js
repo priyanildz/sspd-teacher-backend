1bb1784466f057d442f3cb2846c5ec8e4608bc79
Priyail <nidhipandey8108@gmail.com>
2026-02-18 20:27:51 +0530

    event venue remove

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -45,7 +45,7 @@
 
 
 
-
+const mongoose = require('mongoose'); 
 const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/eventController');
