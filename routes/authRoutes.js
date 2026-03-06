5811f413c43d3403ce894c543ae151e1097d0958
Priyail <nidhipandey8108@gmail.com>
2026-03-06 22:20:44 +0530

    Added examMarks route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, getProfile, getMasterSubjectsByStandard, getStudentsByClass,   // <--- Add this
-  saveExamResult,      // <--- Add this
-  getExamMarks } = require("../controllers/authcontroller");
+const { register, login, getProfile, getMasterSubjectsByStandard } = require("../controllers/authcontroller");
 const authMiddleware = require("../middleware/authMiddleware"); // Middleware to verify JWT
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/profile", authMiddleware, getProfile); // ✅ Add Profile Route
 router.get("/standards/subjects/:standard", getMasterSubjectsByStandard);
-
-router.get("/students", authMiddleware, getStudentsByClass); 
-router.get("/examresults", authMiddleware, getExamMarks);
 
 module.exports = router;
