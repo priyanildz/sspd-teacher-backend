e210e02ffa445e7aaeca1c47f9c54faa22b05dbc
Priyail <nidhipandey8108@gmail.com>
2026-03-14 21:34:15 +0530

    update

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ connectDB();
 app.use(cors({
   origin: "*", 
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization", "auth", "username", "role"]
 }));
 app.use(bodyParser.json());
 
