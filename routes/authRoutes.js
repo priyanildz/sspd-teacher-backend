bfcae04cce6b665336f84a1a4a5c2747fcf84de8
Priyail <nidhipandey8108@gmail.com>
2026-03-16 22:03:00 +0530

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, getProfile, getMasterSubjectsByStandard } = require("../controllers/authcontroller");
+const { register, login, getProfile, getMasterSubjectsByStandard, getTeacherAssignmentOptions } = require("../controllers/authcontroller");
 const authMiddleware = require("../middleware/authMiddleware"); // Middleware to verify JWT
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/profile", authMiddleware, getProfile); // ✅ Add Profile Route
 router.get("/standards/subjects/:standard", getMasterSubjectsByStandard);
+router.get("/teacher-assignment-options", authMiddleware, getTeacherAssignmentOptions);
 
 module.exports = router;
